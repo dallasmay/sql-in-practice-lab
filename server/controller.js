8f17f6d11738cd53e1f2d5d9d41ca9aa2cad8d5c
dallasmay <dmmay9@gmail.com>
2022-06-25 16:29:40 -0600

    built pastAppt functionality, but not showing all past appts for some reason

--- server/controller.js
+++ server/controller.js
@@ -53,11 +53,37 @@ module.exports = {
     },
 
     getPendingAppointments: (req, res) => {
+        // sequelize.query(`select * from cc_appointments
+        // where approved = false
+        // order by date desc;`)
+        //     .then(dbRes => res.status(200).send(dbRes[0]))
+        //     .catch(err => console.log(err))
         sequelize.query(`SELECT * FROM  cc_appointments AS a
                         WHERE approved = false
                         ORDER BY date DESC`
         )
         .then(dbRes => res.status(200).send(dbRes[0]))
         .catch((err) => console.log(err));
+    },
+
+    getPastAppointments: (req, res) => {
+        sequelize.query(`select a.appt_id, a.date, a.service_type, a.notes, u.first_name, u.last_name 
+        from cc_appointments a
+        join cc_emp_appts ea on a.appt_id = ea.appt_id
+        join cc_employees e on e.emp_id = ea.emp_id
+        join cc_users u on e.user_id = u.user_id
+        where a.approved = true and a.completed = true
+        order by a.date desc;`)
+            .then(dbRes => res.status(200).send(dbRes[0]))
+            .catch(err => console.log(err))
+        // sequelize.query(`SELECT a.appt_id, a.date, a.service_type, a.notes, u.first_name, u.last_name
+        //                 FROM cc_users as u
+        //                 JOIN cc_clients AS c ON u.user_id = c.user_id
+        //                 JOIN cc_appointments AS a ON c.client_id = a.client_id
+        //                 WHERE approved = true AND completed = true
+        //                 ORDER BY date DESC`
+        // )
+        // .then((dbRes) => res.status(200).send(dbRes[0]))
+        // .catch((err) => console.log(err))
     }
 }
